184bd89b262fa0516922baa5d138650956529ae0
yujin <yujin.chati@gmail.com>
2023-12-20 09:58:06 +0900

    1230 지도 로드뷰 고도화

--- src/components/sub/contact/Contact.jsx
+++ src/components/sub/contact/Contact.jsx
@@ -5,11 +5,6 @@ import emailjs from '@emailjs/browser';
 
 export default function Contact() {
 	const form = useRef();
-
-	//그룹형식의 DOM을 탐색할때 반환되는 두가지형태의 유사배열
-	//parentDOM.children : HTMLCollection (유사배열: forEach, map 모두 반복불가, Live DOM:상태변경이 실시간)
-	//parentDOM.querySelectorAll : NodeList (유사배열: forEach로는 반복 가능. Static DOM:탐색된 시점의 정적 DOM)
-
 	const resetForm = () => {
 		const elArr = form.current.children;
 
@@ -18,7 +13,6 @@ export default function Contact() {
 			if (el.name === 'user_name' || el.name === 'user_email' || el.name === 'message') el.value = '';
 		});
 	};
-
 	const sendEmail = e => {
 		e.preventDefault();
 
@@ -46,9 +40,9 @@ export default function Contact() {
 	const [Traffic, setTraffic] = useState(false);
 	const [View, setView] = useState(false);
 
+	//참조객체
 	const mapFrame = useRef(null);
 	const viewFrame = useRef(null);
-
 	const marker = useRef(null);
 	const mapInstance = useRef(null);
 
@@ -83,20 +77,24 @@ export default function Contact() {
 		image: new kakao.current.maps.MarkerImage(mapInfo.current[Index].imgSrc, mapInfo.current[Index].imgSize, mapInfo.current[Index].imgOpt)
 	});
 
-	const roadview = useRef(() => {
+	//로드뷰 출력함수
+	const roadview = useCallback(() => {
 		new kakao.current.maps.RoadviewClient().getNearestPanoId(mapInfo.current[Index].latlng, 50, panoId => {
 			new kakao.current.maps.Roadview(viewFrame.current).setPanoId(panoId, mapInfo.current[Index].latlng);
 		});
-	});
-
-	const setCenter = useCallback(() => {
-		mapInstance.current.setCenter(mapInfo.current[Index].latlng);
-		roadview.current();
 	}, [Index]);
 
-	//컴포넌트 마운트시 참조객체에 담아놓은 돔 프레임에 지도 인스턴스 출력 및 마커 세팅
+	//지도위치 가운데 보정 함수
+	const setCenter = useCallback(() => {
+		mapInstance.current.setCenter(mapInfo.current[Index].latlng);
+		//roadview.current();
+	}, [Index]);
+
+	//Index값 변경시마다 지도정보 갱신해서 화면 재랜더링 useEffect
 	useEffect(() => {
+		//Index값이 변경되는 것은 출력할 맵정보가 변경된다는 의미이므로 기존 프레임 안쪽의 정보를 지워서 초기화
 		mapFrame.current.innerHTML = '';
+		viewFrame.current.innerHTML = '';
 		mapInstance.current = new kakao.current.maps.Map(mapFrame.current, {
 			center: mapInfo.current[Index].latlng,
 			level: 3
@@ -105,7 +103,6 @@ export default function Contact() {
 		setTraffic(false);
 		setView(false);
 
-		roadview.current();
 		mapInstance.current.addControl(new kakao.current.maps.MapTypeControl(), kakao.current.maps.ControlPosition.TOPRIGHT);
 		mapInstance.current.addControl(new kakao.current.maps.ZoomControl(), kakao.current.maps.ControlPosition.RIGHT);
 		mapInstance.current.setZoomable(false);
@@ -114,11 +111,19 @@ export default function Contact() {
 		return () => window.removeEventListener('resize', setCenter);
 	}, [Index, setCenter]);
 
+	//Traffic 토글시마다 화면 재랜더링 useEffect
 	useEffect(() => {
 		Traffic
 			? mapInstance.current.addOverlayMapTypeId(kakao.current.maps.MapTypeId.TRAFFIC)
 			: mapInstance.current.removeOverlayMapTypeId(kakao.current.maps.MapTypeId.TRAFFIC);
 	}, [Traffic]);
+
+	//View토글시마다 화면 재랜더링 useEffect
+	useEffect(() => {
+		//view토글시에 무조건 로드뷰정보를 호출하는 것이 아닌 viewFrame안의 내용이 없을때만 호출하고
+		//값이 있을때에는 기존데이터를 재활용해서 불필요한 roadview중복호출을 막음으로서 고용량이 이미지 refetching을 방지
+		View && viewFrame.current.children.length === 0 && roadview();
+	}, [View, roadview]);
 
 	return (
 		<Layout title={'Contact'}>
@@ -157,9 +162,3 @@ export default function Contact() {
 		</Layout>
 	);
 }
-
-/*
-	1.cdn불러온 window에 불러온 외부 객체값을 가져와서 인스턴스 생성
-	2.인스턴스값을 참조객체 담는 이유 (의존성배열에 불필요하게 등록하지 않기 위해서)
-	3.화면변경점이 발생해야 될떄 state값에 따라서 변경되게 로직화한 다음 이벤트 발생시 state를 변경해서 화면 재랜더링 유도
-*/
