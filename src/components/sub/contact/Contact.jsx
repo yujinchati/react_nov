8e088e17f247956cab3534b147209731938b8526
yujin <yujin.chati@gmail.com>
2023-12-22 12:09:31 +0900

    1222 수정사항

--- src/components/sub/contact/Contact.jsx
+++ src/components/sub/contact/Contact.jsx
@@ -35,19 +35,15 @@ export default function Contact() {
 	};
 
 	const kakao = useRef(window.kakao);
-
-	//화면에 출력될 지도정보 배열의 순번이 담길 state
 	const [Index, setIndex] = useState(0);
 	const [Traffic, setTraffic] = useState(false);
 	const [View, setView] = useState(false);
 
-	//참조객체
 	const mapFrame = useRef(null);
 	const viewFrame = useRef(null);
 	const marker = useRef(null);
 	const mapInstance = useRef(null);
 
-	//지점마다 출력할 정보를 개별적인 객체로 묶어서 배열로 그룹화
 	const mapInfo = useRef([
 		{
 			title: '삼성역 코엑스',
@@ -72,28 +68,25 @@ export default function Contact() {
 		}
 	]);
 
-	//마커 인스턴스 생성
 	marker.current = new kakao.current.maps.Marker({
 		position: mapInfo.current[Index].latlng,
 		image: new kakao.current.maps.MarkerImage(mapInfo.current[Index].imgSrc, mapInfo.current[Index].imgSize, mapInfo.current[Index].imgOpt)
 	});
 
-	//로드뷰 출력함수
 	const roadview = useCallback(() => {
 		new kakao.current.maps.RoadviewClient().getNearestPanoId(mapInfo.current[Index].latlng, 50, panoId => {
 			new kakao.current.maps.Roadview(viewFrame.current).setPanoId(panoId, mapInfo.current[Index].latlng);
 		});
 	}, [Index]);
 
-	//지도위치 가운데 보정 함수
 	const setCenter = useCallback(() => {
+		console.log('setCenter');
 		mapInstance.current.setCenter(mapInfo.current[Index].latlng);
 		//roadview.current();
 	}, [Index]);
 
 	const throttledSetCenter = useThrottle(setCenter);
 
-	//Index값 변경시마다 지도정보 갱신해서 화면 재랜더링 useEffect
 	useEffect(() => {
 		//Index값이 변경되는 것은 출력할 맵정보가 변경된다는 의미이므로 기존 프레임 안쪽의 정보를 지워서 초기화
 		mapFrame.current.innerHTML = '';
@@ -109,22 +102,22 @@ export default function Contact() {
 		mapInstance.current.addControl(new kakao.current.maps.MapTypeControl(), kakao.current.maps.ControlPosition.TOPRIGHT);
 		mapInstance.current.addControl(new kakao.current.maps.ZoomControl(), kakao.current.maps.ControlPosition.RIGHT);
 		mapInstance.current.setZoomable(false);
+	}, [Index]);
 
+	//윈도우 객체에 등록할 핸들러함수를 따로 useEffect로 빼놓은 다음 의존성 배열을 가급적 비워두는 것이 좋음
+	//윈도우 등록되는 핸들러함수에 만약 특정 state값을 의존한다면은 연결되는 함수이름자체를 의존성배열에 등록하되 useCallback처리
+	useEffect(() => {
 		window.addEventListener('resize', throttledSetCenter);
 		return () => window.removeEventListener('resize', throttledSetCenter);
-	}, [Index, throttledSetCenter]);
+	}, [throttledSetCenter]);
 
-	//Traffic 토글시마다 화면 재랜더링 useEffect
 	useEffect(() => {
 		Traffic
 			? mapInstance.current.addOverlayMapTypeId(kakao.current.maps.MapTypeId.TRAFFIC)
 			: mapInstance.current.removeOverlayMapTypeId(kakao.current.maps.MapTypeId.TRAFFIC);
 	}, [Traffic]);
 
-	//View토글시마다 화면 재랜더링 useEffect
 	useEffect(() => {
-		//view토글시에 무조건 로드뷰정보를 호출하는 것이 아닌 viewFrame안의 내용이 없을때만 호출하고
-		//값이 있을때에는 기존데이터를 재활용해서 불필요한 roadview중복호출을 막음으로서 고용량이 이미지 refetching을 방지
 		View && viewFrame.current.children.length === 0 && roadview();
 	}, [View, roadview]);
 
